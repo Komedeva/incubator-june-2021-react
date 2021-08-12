e99d6a56dee16eaaf65958e8f07a723ce2c0fd0f
Huawei <katya.komedeva00@gmail.com>
2021-08-12 18:31:24 +0300

    home work React2

---
+++ src/users/Users.js
@@ -0,0 +1,18 @@
+import {useEffect, useState} from "react";
+import {User} from '../user/User';
+import {getUsers} from '../../services/user.service.js';
+
+
+export default function Users(){
+    let [users, setUsers]= useState([]);
+
+    useEffect(() => {
+        getUsers().then(value => setUsers([...value]));
+    }, []);
+
+    return (
+        <div>
+            {users.map(value => <User key={value.id} user={value}/>) }
+        </div>
+    );
+}
