f0a2f8cde5f99258b9e3d2bda7cae79a01679adf
Huawei <katya.komedeva00@gmail.com>
2021-08-13 17:33:45 +0300

    home work React3

--- src/posts/Posts.js
+++ src/posts/Posts.js
@@ -1,11 +1,12 @@
 import {Post} from "../post/Post";
-import {useState} from "react";im
-import
+import {useState} from "react";
+import {getPosts} from '../service/PostService';
+import {getPost} from '../service/PostService';
+
 let [Posts, setPosts]= useState(null);
 
 <div className={'posts'}>
-    {
-        posts && posts.map(value =>
+    {posts && posts.map(value =>
             <Post
                 key={value.id}
                 post={value}/>
