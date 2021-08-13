7727a92edd46d12fdd288dc881cf5a2d4cb97e3b
Huawei <katya.komedeva00@gmail.com>
2021-08-13 14:47:48 +0300

    home work React2

---
+++ src/service/user-post.js
@@ -0,0 +1,11 @@
+function getUsers() {
+    return fetch('https://jsonplaceholder.typicode.com/users')
+        .then(value=> value.json());
+}
+
+function getPostOfUser(id){
+    return fetch('https://jsonplaceholder.typicode.com/users/'+id+'/posts')
+        .then(value=> value.json());
+}
+
+export {getUsers, getPostOfUser};
