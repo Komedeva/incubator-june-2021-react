3c109430c19bee46272e55a1c2180d46fc42264d
Huawei <katya.komedeva00@gmail.com>
2021-08-18 15:04:29 +0300

    car React4

--- src/components/cars.service/CarService.js
+++ src/components/cars.service/CarService.js
@@ -1,5 +1,41 @@
-const car1 = ({model: model, price: price, year: year}) => {
 
+//     fetch('http://195.72.146.25/api/v1/cars', {
+// }
+//     method: 'DELETE',
+// });
+
+// function deleteCar(){
+//     return fetch('http://195.72.146.25/api/v1/cars/45', {
+//     method: 'DELETE',
+//         headers: {
+//             'Content-type': 'application/json; charset=UTF-8',
+//         },
+//         .then(value=> value.json())
+// };
+
+
+function getCars(){
+    return fetch('http://195.72.146.25/api/v1/cars')
+        .then(value => value.json())
+}
+
+function carPatch({model: model, price: price, year: year}){
+    return fetch('http://195.72.146.25/api/v1/cars', {
+    method: 'PATCH',
+        body: JSON.stringify({
+             model, price, year
+    }),
+    headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+    },
+})
+    .then((response) => response.json())
+    .then((json) => console.log(json));
+}
+
+
+
+const car1 = ({model: model, price: price, year: year}) => {
     return fetch('http://195.72.146.25/api/v1/cars', {
         method: 'POST',
         body: JSON.stringify({
@@ -14,4 +50,4 @@ const car1 = ({model: model, price: price, year: year}) => {
         .then((response) => response.json())
         .then((json) => console.log(json));
 }
-export {car1}
+export {car1,getCars, carPatch}
--- src/components/controlledform/ControlledForm.js
+++ src/components/controlledform/ControlledForm.js
@@ -1,41 +1,59 @@
-import {useState} from "react";
-import {car1} from "../cars.service/CarService";
+import {useEffect, useState} from "react";
+import {car1, getCars} from "../cars.service/CarService";
+// import './Carstyle.css'
+// import {deleteCar} from './../cars.service';
+import {carPatch} from './../cars.service/CarService'
 
 export default function ControlledForm() {
-    let [model, setModel]= useState('');
-    let [price, setPrice]= useState('');
-    let [year, setYear]= useState('');
-    let [car, setCar]= useState({model:'', price:'', year:''})
+    let [model, setModel] = useState('');
+    let [price, setPrice] = useState('');
+    let [year, setYear] = useState('');
+    let [car, setCar] = useState({model: '', price: '', year: ''});
+    let [cars, setCars] = useState([]);
+    let [carpatch, setcarpatch]= useState('');
 
+
+    useEffect(() => {
+        getCars().then(value => setCars([...value]))
+    })
+
+
+    useEffect(()=>{
+        carPatch().then(value => setcarpatch([...value]))
+    })
 
     const onsubmitContForm = (e) => {
         e.preventDefault();
 
-        let tempCar= {model, price, year}
+        let tempCar = {model, price, year}
         setCar({...tempCar})
         car1(tempCar).then(value => console.log(value));
     }
 
-    const onInputChangeModel= (e)=>{
+    const onInputChangeModel = (e) => {
         setModel(e.target.value);
     }
 
-    const onInputChangePrice= (e)=>{
+    const onInputChangePrice = (e) => {
         setPrice(e.target.value);
     }
 
-    const onInputChangeYear= (e)=>{
+    const onInputChangeYear = (e) => {
         setYear(e.target.value);
     }
 
-    return(
-    <div>
-    <form onSubmit={onsubmitContForm}>
-        <input type="text" name={model} value={model} onInput={onInputChangeModel}/>
-        <input type="text" name={price} value={price} onInput={onInputChangePrice}/>
-        <input type="text" name={year} value={year} onInput={onInputChangeYear}/>
-        <input type="submit" value={'save'}/>
-    </form>
-    </div>
-        )
+    return (
+        <div>
+
+            {cars.map(value => <div className='Carmodel'
+                                    key={value.id}> Model: {value.model}, Price: {value.price}, Year: {value.year}</div>)}
+
+            <form onSubmit={onsubmitContForm}>
+                <input type="text" name={model} value={model} onInput={onInputChangeModel}/>
+                <input type="text" name={price} value={price} onInput={onInputChangePrice}/>
+                <input type="text" name={year} value={year} onInput={onInputChangeYear}/>
+                <input type="submit" value={'save'}/>
+            </form>
+        </div>
+    )
 }
