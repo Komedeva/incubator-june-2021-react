4bbaf918c9a7a5f9973e68ed4e60fc2b56689b3f
Huawei <katya.komedeva00@gmail.com>
2021-08-19 16:57:48 +0300

    HW about links React lesson5

---
+++ src/car/Car.js
@@ -0,0 +1,16 @@
+import {useEffect, useState} from "react";
+import {car} from "../service/service.car";
+import {getCars} from "../service/service.car";
+
+export default function ControlledForm() {
+    let [model, setModel] = useState('');
+    let [price, setPrice] = useState('');
+    let [year, setYear] = useState('');
+    let [cars, setCars] = useState([]);
+}
+
+function getCar(e){
+return(
+
+)
+}
