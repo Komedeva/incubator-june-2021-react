7872e537ce3839ab4b794af2c858b2aac14ff3b1
Huawei <katya.komedeva00@gmail.com>
2021-08-26 17:11:02 +0300

    6lessonHW

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import {useReducer} from "react";
 import './App.css'
-const reducer = (state = {a: 0, b: 0, c: 0}, action) => {
-    if (action.obj === '1' && action.click === '+') {
-        return {...state, a: state.a++}
-    } else if (action.obj === '1' && action.click === '-') {
-        return {...state, a: state.a--}
 
-        } else if (action.obj === '2' && action.click === '-') {
-            return {...state, b: state.b--}
-        } else if (action.obj === '2' && action.click === '+') {
-            return {...state, b: state.b++}
-
-        } else if (action.obj === '3' && action.click === '-') {
-            return {...state, c: state.c--}
-        } else if (action.obj === '3' && action.click === '+') {
-            return {...state, c: state.c++}
-        return {...state};
+const reducer = (state, action) => {
+    switch (action.click) {
+        case '+':
+            return {...state, [action.obj]: state[action.obj] + 1};
+            // return {...state, a: state.a -1}
+        case '-':
+            return {...state, [action.obj]: state[action.obj] - 1};
+        default:
+            return state
     }
 }
+
 export default function App() {
     let [state, dispatch] = useReducer(reducer, {a: 0, b: 0, c: 0});
     let {a, b, c} = state;
@@ -26,18 +21,18 @@ export default function App() {
     return (
         <div>
             <h2>state 1 - {a} </h2>
-            <button className="button is-success is-light" onClick={() => dispatch({obj: '1', click: '-'})}>minus
+            <button className="button is-success is-light" onClick={() => dispatch({obj: 'a', click: '-'})}>minus
             </button>
-            <button className="button is-success is-light" onClick={() => dispatch({obj: '1', click: '+'})}>plus
+            <button className="button is-success is-light" onClick={() => dispatch({obj: 'a', click: '+'})}>plus
             </button>
             <h2>state 2 - {b} </h2>
-            <button className="button is-warning is-light" onClick={() => dispatch({obj: '2', click: '-'})}>minus
+            <button className="button is-warning is-light" onClick={() => dispatch({obj: 'b', click: '-'})}>minus
             </button>
-            <button className="button is-warning is-light" onClick={() => dispatch({obj: '2', click: '+'})}>plus
+            <button className="button is-warning is-light" onClick={() => dispatch({obj: 'b', click: '+'})}>plus
             </button>
             <h2>state 1 - {c} </h2>
-            <button className="button is-info is-light" onClick={() => dispatch({obj: '3', click: '-'})}>minus</button>
-            <button className="button is-info is-light" onClick={() => dispatch({obj: '3', click: '+'})}>plus</button>
+            <button className="button is-info is-light" onClick={() => dispatch({obj: 'c', click: '-'})}>minus</button>
+            <button className="button is-info is-light" onClick={() => dispatch({obj: 'c', click: '+'})}>plus</button>
 
         </div>
     );
