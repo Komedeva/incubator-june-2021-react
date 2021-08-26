84df978129405c79113d42b22097b8821d3eadb4
Huawei <katya.komedeva00@gmail.com>
2021-08-26 13:45:33 +0300

    6lessonHW

--- src/App.js
+++ src/App.js
@@ -1,7 +1,56 @@
-export default function App() {
-  return (
-    <div>
-      App
-    </div>
-  );
+import {useReducer} from "react";
+
+const reducer = (state = 0, action) => {
+    switch (action) {
+        case '+':
+            return state + 10;
+        case '-':
+            return state - 2;
+        default:
+            return state
+    }
 }
+
+
+export default function App() {
+
+    let [state, dispatch] = useReducer(reducer)
+    console.log(state);
+
+    return (
+        <div>
+            <h1>{state}</h1>
+            <button onClick={()=>dispatch('+')}>plus</button>
+            <button onClick={()=>dispatch('-')}>minus</button>
+        </div>
+    );
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
