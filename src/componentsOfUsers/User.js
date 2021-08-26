9f51c98ab20bcbd0af7bc62e561efb52192fe917
Huawei <katya.komedeva00@gmail.com>
2021-08-26 14:26:30 +0300

    6lessonHW

--- src/componentsOfUsers/User.js
+++ src/componentsOfUsers/User.js
@@ -4,13 +4,15 @@ import {Route} from "react-router-dom";
 
 function User({user, history}) {
     const navigateTo = () => {
-        history.push(`/usersPage/${user.id}`);
+        history.push({
+            pathname: `/usersPage/${user.id}`,
+            state: user
+        });
     }
-            console.log(user)
     return (
         <div>
-            {user.name} - <button onClick={UserDetails}>show details</button>
-            {/*<Route component={UserDetails}/>*/}
+            {user.name} - <button onClick={ navigateTo}>show details</button>
+            <Route path={`/usersPage/${user.id}`} component={UserDetails}/>
         </div>
     )
 }
--- src/componentsOfUsers/UserDetails.js
+++ src/componentsOfUsers/UserDetails.js
@@ -1,24 +1,24 @@
 export default function UserDetails(props) {
-let {match:{params:{id}}}= props;
-     console.log(id);
-     return (
+     console.log(props);
+
+    const {state: user} = props.location;
+    return (
         <div>
-             Lorem ipsum dolor sit amet.
-             {/*<h1>{user.id} {user.name} {user.username}</h1>*/}
-             {/*<p>Email:{user.email}</p>*/}
-             {/*<p>Phone:{user.phone}</p>*/}
-             {/*<p>Website:{user.website}</p>*/}
-             {/*<h4>Address:</h4>*/}
-             {/*<p>street:{user.address.street}</p>*/}
-             {/*<p>suite:{user.address.suite}</p>*/}
-             {/*<p>city:{user.address.city}</p>*/}
-             {/*<h4>Company:</h4>*/}
-             {/*<p>name:{user.company.name}</p>*/}
-             {/*<p>bs:{user.company.bs}</p>*/}
-             {/*<p>catchPhrase:{user.company.catchPhrase}</p>*/}
-             {/*<h4>Geo:</h4>*/}
-             {/*<p>lat:{user.address.geo.lat}</p>*/}
-             {/*<p>lng:{user.address.geo.lng}</p>*/}
+             <h3>{user.id} {user.name} {user.username}</h3>
+             <p>Email:{user.email}</p>
+             <p>Phone:{user.phone}</p>
+             <p>Website:{user.website}</p>
+                <h4>Address:</h4>
+                <p>street:{user.address.street}</p>
+                <p>suite:{user.address.suite}</p>
+                <p>city:{user.address.city}</p>
+             <h4>Company:</h4>
+             <p>name:{user.company.name}</p>
+             <p>bs:{user.company.bs}</p>
+             <p>catchPhrase:{user.company.catchPhrase}</p>
+             <h4>Geo:</h4>
+             <p>lat:{user.address.geo.lat}</p>
+             <p>lng:{user.address.geo.lng}</p>
         </div>
     );
 }
