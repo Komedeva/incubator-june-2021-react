b8d1d0605136fa2dfb19864a2b33025ded5ea1dd
Huawei <katya.komedeva00@gmail.com>
2021-08-31 20:04:29 +0300

    6lessonHW

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import {useDispatch, useSelector} from "react-redux";
+
 export default function App() {
-  return (
-    <div>
-      App
-    </div>
-  );
+    const dispatch = useDispatch();
+    const cash = useSelector(state => state.cash)
+    console.log(cash)
+
+    const getCash = () => {
+        dispatch({type:"GET_CASH", payload:10 })
+    }
+
+    const addCash = () => {
+        dispatch({type:"ADD_CASH", payload:5})
+    }
+
+
+    return (
+        <div style={{displey: "flex"}}>
+            <button onClick={() => getCash()}>GET CASH</button>
+            <button onClick={() => addCash()}>ADD CASH</button>
+        </div>
+    );
 }
--- src/index.js
+++ src/index.js
@@ -3,12 +3,37 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {createStore} from "redux";
+import {Provider} from "react-redux";
+
+const defaultState = {
+    cash: 0
+}
+
+const reducer = (state = defaultState, action) => {
+    switch (action.type) {
+        case "ADD_CASH":
+            return {...state, cash: state.cash + action.payload}
+
+        case "GET_CASH":
+            return {...state, cash: state.cash - action.payload}
+
+        default:
+            return state;
+    }
+}
+
+const store = createStore(reducer)
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+
+        <Provider store={store}>
+
+            <App/>
+        </Provider>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
