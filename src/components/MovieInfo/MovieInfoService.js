96316ac4b8b2e410aa5988c9f51ccd33bb9ac7ef
Huawei <katya.komedeva00@gmail.com>
2021-09-09 18:50:13 +0300

    project react

--- src/components/MovieInfo/MovieInfoService.js
+++ src/components/MovieInfo/MovieInfoService.js
@@ -1,5 +1,5 @@
 function MovieInfoService(id) {
-    return fetch('https://api.themoviedb.org/3/movie' + 'id', {
+    return fetch('https://api.themoviedb.org/3/movie' +'/' + 'id', {
         headers: {
             Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhMzMxNWJlYWY2NmQ1Y2E0ZTM4MTI2YzRlMzVjOGFjMyIsInN1YiI6IjYxMmY2NWMyMTIxOTdlMDAyYWMwOGUzYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.4MsMLNzvs84P0C9DOu8ws5b_2wv6DKO2Y5HhabvciuQ'
         }
@@ -12,11 +12,6 @@ function MovieInfoService(id) {
 export {MovieInfoService};
 
 
-const getUser = (id) => {
-    return fetch(url + '/' + id)
-        .then(value => value.json());
-
-}
 //
 // import axios from "axios";
 //
